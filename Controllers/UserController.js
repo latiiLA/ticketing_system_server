667cf838d8774ce58bb3ccdadc5b28945027fab0
Lata <amelatbay@gmail.com>
2025-03-07 16:21:37 +0300

    create and edit ticket updated

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,6 +1,7 @@
 import User from "../Models/userModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import Ticket from "../Models/ticketModel.js";
 
 // create json web token
 const maxAge = 1 * 24 * 60 * 60;
@@ -151,4 +152,135 @@ const loginUser = async (req, res) => {
   }
 };
 
-export { createUser, loginUser };
+const createTicket = async (req, res) => {
+  try {
+    const auth_user = req.auth_user;
+    console.log("inside ticket", req.body);
+    const { title, description, status } = req.body;
+
+    const new_ticket = await Ticket.create({
+      title,
+      description,
+      status,
+      createdBy: auth_user._id,
+    });
+
+    console.log(new_ticket);
+    res.status(200).json({
+      status: "success",
+      new_ticket,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: false,
+      message: "Failed to retrieve create ticket",
+      error: error.message,
+    });
+  }
+};
+
+const getMyTickets = async (req, res) => {
+  try {
+    const auth_user = req.auth_user;
+    let role = req.auth_user.role;
+
+    // Fetch my tickets
+    const mytickets = await Ticket.find({
+      isDeleted: false,
+      createdBy: auth_user._id,
+    }).populate({
+      path: "createdBy",
+      select: "firstName lastName",
+    });
+
+    console.log("get mytickets", mytickets);
+
+    res.status(200).json({
+      status: "true",
+      mytickets,
+    });
+  } catch (error) {
+    console.error("Error fetching mytickets:", error);
+    res.status(500).json({
+      status: "false",
+      message: "Internal server error",
+    });
+  }
+};
+
+const getAllTickets = async (req, res) => {
+  try {
+    const auth_user = req.auth_user;
+    let role = req.auth_user.role;
+
+    // Fetch all tickets
+    const alltickets = await Ticket.find({
+      isDeleted: false,
+    }).populate({
+      path: "createdBy",
+      select: "firstName lastName",
+    });
+
+    console.log("get alltickets", alltickets);
+
+    res.status(200).json({
+      status: "true",
+      alltickets,
+    });
+  } catch (error) {
+    console.error("Error fetching mytickets:", error);
+    res.status(500).json({
+      status: "false",
+      message: "Internal server error",
+    });
+  }
+};
+
+const editTicket = async (req, res) => {
+  const { id } = req.params;
+  const updateData = req.body;
+  const auth_user = req.auth_user;
+
+  try {
+    // Find the existing ticket
+    const existingTicket = await Ticket.findById(id);
+    if (!existingTicket) {
+      return res.status(404).json({ message: "Ticket not found." });
+    }
+
+    // Update the ticket with the new data
+    let updatedTicket = await Ticket.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedTicket) {
+      return res.status(404).json({ message: "Ticket not found." });
+    }
+
+    res.status(200).json({
+      message: "Ticket successfully updated.",
+      updatedTicket,
+    });
+  } catch (error) {
+    if (error.code === 11000) {
+      // Duplicate key error
+      return res.status(400).json({ message: "Duplicate key error", error });
+    }
+    console.error("Error updating ticket:", error);
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export {
+  createUser,
+  loginUser,
+  createTicket,
+  getMyTickets,
+  getAllTickets,
+  editTicket,
+};
