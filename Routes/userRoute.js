7d2a192a6b166fe43a280e92ac88859a67947af7
Lata <amelatbay@gmail.com>
2025-03-07 16:22:47 +0300

    user route added

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -2,13 +2,35 @@ import express from "express";
 const app = express();
 const router = express.Router();
 
-import { createUser, loginUser } from "../Controllers/userController.js";
+import {
+  createTicket,
+  createUser,
+  loginUser,
+  getMyTickets,
+  getAllTickets,
+  editTicket,
+} from "../Controllers/userController.js";
 import isAuthenticated from "../Middlewares/authenticate.js";
-// import { isAuthorized } from "../Middlewares/authorization.js";
+import {
+  isAuthorized,
+  isUserAuthorized,
+} from "../Middlewares/authorization.js";
 
 // CREATE USER
 router.route("/signup").post(createUser);
 // Login
 router.route("/login").post(loginUser);
+// Create ticket
+router
+  .route("/tickets")
+  .post(isAuthenticated, isUserAuthorized(), createTicket);
+// Edit ticket
+router.route("/tickets/:id").put(isAuthenticated, isAuthorized(), editTicket);
+// Get my tickets for user
+router
+  .route("/mytickets")
+  .get(isAuthenticated, isUserAuthorized(), getMyTickets);
+// Get all tickets for admin
+router.route("/alltickets").get(isAuthenticated, isAuthorized(), getAllTickets);
 
 export default router;
